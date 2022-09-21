606ff48ab56ffd6b4db1b30de869de3e746d2df9
Leonardo Anzulin <leoanzulin@gmail.com>
2022-09-21 19:04:03 -0300

    primeiro exercicio

---
+++ fundamentos/secao-3-introducao_a_javascript/dia-1-primeiros_passos/exercicio1.js
@@ -0,0 +1,17 @@
+const a = 10;
+const b = 5;
+let adicao;
+let subtracao;
+let multiplicacao;
+let divisao;
+let modulo;
+adicao = a + b;
+subtracao = a - b;
+multiplicacao = a * b;
+divisao = a / b;
+modulo = a % b;
+console.log(adicao);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+console.log(modulo);
