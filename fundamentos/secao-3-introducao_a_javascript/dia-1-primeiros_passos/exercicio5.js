58923cbccb48a3dfdd50be25f916f5b324afd85a
Leonardo Anzulin <leoanzulin@gmail.com>
2022-09-21 19:38:07 -0300

    exercicio 5

---
+++ fundamentos/secao-3-introducao_a_javascript/dia-1-primeiros_passos/exercicio5.js
@@ -0,0 +1,14 @@
+const a = 170;
+const b = 2;
+const c = 25;
+if (a > 0 && b > 0 && c >0){
+    if ((a+b+c) <= 180){
+        console.log(true)
+    }
+    else{
+        console.log(false)
+    }
+}
+else {
+    console.log("possui ângulo negativo")
+}
