db913c40fd9c6795c9a265503348636cab9a3b85
Leonardo Anzulin <leoanzulin@gmail.com>
2022-09-30 16:09:43 -0300

    comitão bomba

---
+++ fundamentos/secao-4-javascript/dia-3-eventos/main.js
@@ -0,0 +1,56 @@
+const firstLi = document.getElementById('first-li');
+const secondLi = document.getElementById('second-li');
+const thirdLi = document.getElementById('third-li');
+const input = document.getElementById('input');
+const myWebpage = document.getElementById('my-spotrybefy');
+
+function adicionaFirstClass() {
+    firstLi.className = 'tech';
+}
+firstLi.addEventListener("click", adicionaFirstClass);
+
+function caixaTexto() {
+    document.getElementsByClassName('tech')[0].innerText = 'alterando o texto';
+}
+input.addEventListener("change", caixaTexto);
+
+function top3() {
+    window.open("https://leoanzulin.github.io/")
+}
+myWebpage.addEventListener("click", top3);
+
+function changeColor() {
+    myWebpage.style.color = "red"
+}
+myWebpage.addEventListener("mouseover", changeColor);
+// - Copie esse arquivo e edite apenas ele;
+//  - Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
+
+// - Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
+//  - Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+// - Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
+// com a classe 'tech';
+
+// - Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
+// redirecione para alguma página;
+//  - Que tal redirecionar para seu portfólio?
+
+// - Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
+// a cor do mesmo;
+
+// Segue abaixo um exemplo do uso de event.target:
+
+
+function resetText(event) {
+  // O Event é passado como um parâmetro para a função.
+  event.target.innerText = 'Opção reiniciada';
+  // O event possui várias propriedades, porém a mais usada é o event.target,
+  // que retorna o objeto que disparou o evento.
+}
+
+firstLi.addEventListener('dblclick', resetText);
+
+// Não precisa passar o parâmetro dentro da callback resetText. O próprio
+// navegador fará esse trabalho por você, não é legal? Desse jeito, o
+// event.target na nossa função retornará o objeto 'firstLi'.
