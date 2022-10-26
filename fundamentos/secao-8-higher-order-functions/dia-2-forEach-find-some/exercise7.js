3b0b072685b6d88d48e93e33b1fe720782d2da2a
Leonardo Anzulin <leoanzulin@gmail.com>
2022-10-26 09:34:48 -0300

    exercise 7

---
+++ fundamentos/secao-8-higher-order-functions/dia-2-forEach-find-some/exercise7.js
@@ -0,0 +1,8 @@
+const books = require('./dataBase');
+const expectedResult = true;
+
+function someBookWasReleaseOnThe80s() {
+  return books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
+}
+
+console.log(someBookWasReleaseOnThe80s());
